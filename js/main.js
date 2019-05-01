92919665c655cd2fa821bc18fd1981648f7c08a6
prashadi <madushiprashadi@gmail.com>
2019-05-01 16:25:26 +0530

    seconds circle is done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(400, 400);
 }
 
 function draw() {
     background(255);
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
+    translate(200,200);
+    let hr = hour();
+    let mn = minute();
+    let sc = second();
+    strokeWeight(2);
+    stroke(0);
+    fill(0);
+    circle(0,0,300);
+    let sc_color = map(sc,0,60,0,255);
+    fill(0,0,sc_color);
+    let sc_map = map(sc,0,60,250,300);
+    circle(0,0,sc_map);
 }
