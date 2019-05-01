69e9d8f9927878e50e9f5e26e83ee92173d54ef6
prashadi <madushiprashadi@gmail.com>
2019-05-01 16:46:32 +0530

    add offset for color. hour and minute added.

--- js/main.js
+++ js/main.js
@@ -8,12 +8,20 @@ function draw() {
     let hr = hour();
     let mn = minute();
     let sc = second();
-    strokeWeight(2);
-    stroke(0);
+    strokeWeight(4);
+    stroke(255);
     fill(0);
     circle(0,0,300);
-    let sc_color = map(sc,0,60,0,255);
+    let sc_color = map(sc,0,60,100,255);
     fill(0,0,sc_color);
-    let sc_map = map(sc,0,60,250,300);
+    let sc_map = map(sc,0,60,200,300);
     circle(0,0,sc_map);
+    let mn_color = map(mn,0,60,100,255);
+    fill(0,mn_color,0);
+    let mn_map = map(mn,0,60,100,200);
+    circle(0,0,mn_map);
+    let hr_color = map(hr,0,24,100,255);
+    fill(hr_color,0,0);
+    let hr_map = map(hr,0,24,0,100);
+    circle(0,0,hr_map);
 }
